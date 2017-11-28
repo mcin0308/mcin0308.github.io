2844fe3f048fecd3ccd332a78a184ee202027a1a
mcin0308 <32109853+mcin0308@users.noreply.github.com>
2017-11-28 09:13:40 -0500

    First Upload

---
+++ onewebpage/js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+	//material contact form
+	$('.contact-form').find('.form-control').each(function() {
+	  var targetItem = $(this).parent();
+	  if ($(this).val()) {
+		$(targetItem).find('label').css({
+		  'top': '10px',
+		  'fontSize': '14px'
+		});
+	  }
+	})
+	$('.contact-form').find('.form-control').focus(function() {
+	  $(this).parent('.input-block').addClass('focus');
+	  $(this).parent().find('label').animate({
+		'top': '10px',
+		'fontSize': '14px'
+	  }, 300);
+	})
+	$('.contact-form').find('.form-control').blur(function() {
+	  if ($(this).val().length == 0) {
+		$(this).parent('.input-block').removeClass('focus');
+		$(this).parent().find('label').animate({
+		  'top': '25px',
+		  'fontSize': '18px'
+		}, 300);
+	  }
+	})
+
+});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
